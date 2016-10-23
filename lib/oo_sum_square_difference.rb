2d0559d269e02986b6079fe6f8a3401de742f671
Robert Hitt <robhitt@gmail.com>
2016-10-23 14:26:28 +0000

    Done.

--- lib/oo_sum_square_difference.rb
+++ lib/oo_sum_square_difference.rb
@@ -1,1 +1,34 @@
 # Implement your object-oriented solution here!
+# Implement your procedural solution here!
+class SumSquareDifference
+    def initialize(num)
+      @num = num
+    end
+
+    def sum_of_squares(num)
+      x = 1
+    	new_num = 0
+
+    	while x < (num + 1)
+    		new_num += x ** 2
+      	x += 1
+    	end
+    	new_num
+  end
+
+
+  def square_of_sums(num)
+    x = 1
+    new_num = 0
+
+    while x < (num + 1)
+    	new_num += x
+      x += 1
+    end
+    new_num ** 2
+  end
+
+  def difference
+  	square_of_sums(@num) - sum_of_squares(@num)
+  end
+end
--- lib/sum_square_difference.rb
+++ lib/sum_square_difference.rb
@@ -1,1 +1,27 @@
 # Implement your procedural solution here!
+def sum_of_squares(num)
+	x = 1
+  	new_num = 0
+
+  	while x < (num + 1)
+  		new_num += x ** 2
+    	x += 1
+  	end
+  	new_num
+end
+
+
+def square_of_sums(num)
+  x = 1
+  new_num = 0
+
+  while x < (num + 1)
+  	new_num += x
+    x += 1
+  end
+  new_num ** 2
+end
+
+def sum_square_difference(num)
+	square_of_sums(num) - sum_of_squares(num)
+end
